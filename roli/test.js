bb56c60c6aaab4170d32936c3b31b4136dcae682
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-09-11 06:22:45 +0700

    update request and fix error

--- roli/test.js
+++ roli/test.js
@@ -1,17 +1,16 @@
 #!node
 
+const { config } = require("./constant");
 const { decrypt, encrypt } = require("./decode");
-const { tokenize, config, login, relogin } = require("./util");
+const { tokenize, login, relogin } = require("./util");
 
 /* testing custom request */
 (async () => {
   const user = await login()
-  const token = encrypt(tokenize("daily_reward/info_indirect"))
+  const token = encrypt(tokenize("daily_reward/check_status"))
   const data = {
     "key":config.key,
-    "counter":"29",
     "session":user.session,
-    "user_id":user.data.id,
     "token":token,
     "msisdn":user.data.msisdn.replace(/^0/, '62'),
     "id_user":user.data.id,
@@ -20,7 +19,7 @@ const { tokenize, config, login, relogin } = require("./util");
   }
   const reqparam = encrypt(JSON.stringify(data))
   try {
-    const response = await fetch("https://roli.telkomsel.com/api/daily_reward/info_indirect?" + `key=m0b1l3&token=${token}&version_number=3.0.6&reqparam=${reqparam}`).then(res => res.text())
+    const response = await fetch("https://roli.telkomsel.com/api/daily_reward/check_status?" + `key=m0b1l3&token=${token}&version_number=3.0.6&reqparam=${reqparam}`).then(res => res.text())
     const result = decrypt(response)
     console.log(decrypt(response));
     if (result.message == "User not found.") {
