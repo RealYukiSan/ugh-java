5ecfdde51f53ee2940fab644b5044c9e8c51e5f1
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-09-12 19:27:37 +0700

    example with internal_counter/my_counter

--- roli/test.js
+++ roli/test.js
@@ -7,18 +7,7 @@ const { tokenize, login, relogin } = require("./util");
 /* testing custom request */
 (async () => {
   const user = await login()
-  const token = encrypt(tokenize("optin/update_counter"))
-  const form = new FormData()
-  const reqparampost = encrypt(JSON.stringify({
-    "key":config.key,
-    "session":user.session,
-    "user_id":user.data.id,
-    "postr_user_hash":"null",
-    "postr_user_id":"null",
-    "counter_value":"10",
-    "tgl_counter":"12-09-2023"
-  }))
-  form.append("reqparampost", reqparampost)
+  const token = encrypt(tokenize("internal_counter/my_counter"))
   const data = {
     "key":config.key,
     "session":user.session,
@@ -30,10 +19,7 @@ const { tokenize, login, relogin } = require("./util");
   }
   const reqparam = encrypt(JSON.stringify(data))
   try {
-    const response = await fetch("https://roli.telkomsel.com/api/optin/update_counter?" + `key=m0b1l3&token=${token}&version_number=3.0.6&reqparam=${reqparam}`, {
-      method: "POST",
-      body: form
-    }).then(res => res.text())
+    const response = await fetch("https://roli.telkomsel.com/api/internal_counter/my_counter?" + `key=m0b1l3&token=${token}&version_number=3.0.6&reqparam=${reqparam}`).then(res => res.text())
     const result = decrypt(response)
     console.log(decrypt(response));
     if (result.message == "User not found.") {
