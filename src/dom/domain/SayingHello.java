38845f065836471b2e290b4e68d714eedd053c16
reyuki <hikikomori.yukisan@gmail.com>
2024-02-16 20:37:52 +0700

    reformat

--- src/dom/domain/SayingHello.java
+++ src/dom/domain/SayingHello.java
@@ -1,13 +1,14 @@
 package dom.domain;
 import android.widget.TextView;
+import android.app.Activity;
 
-public final class SayingHello extends android.app.Activity
+public final class SayingHello extends Activity
 {
-    protected @Override void onCreate( final android.os.Bundle activityState )
+    protected @Override void onCreate(final android.os.Bundle activityState)
     {
-        super.onCreate( activityState );
-        final TextView textV = new TextView( SayingHello.this );
-        textV.setText( "Hello world" );
-        setContentView( textV );
+        super.onCreate(activityState);
+        final TextView textV = new TextView(this);
+        textV.setText("Hello world");
+        setContentView(textV);
     }
 }
