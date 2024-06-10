f5ba58899bf54d0fdd2c3899c75c299f2df25a1a
reyuki <hikikomori.yukisan@gmail.com>
2024-06-10 15:18:26 +0700

    update uses-sdk tag, so no need to add --bypass-low-target-sdk-block flag any more, and add some note
    TODO: figure out why the apk are considered as old version

--- hello-jni/jni/hello-jni.c
+++ hello-jni/jni/hello-jni.c
@@ -1,5 +1,7 @@
 #include <jni.h>
 
+// did you wonder why the name sucks?
+// read this: https://stackoverflow.com/questions/32470463/what-is-the-naming-convention-for-java-native-interface-method-and-module-name
 jstring Java_com_example_hellojni_HelloJni_stringFromJNI(JNIEnv* env, jobject thiz)
 {
   return (*env)->NewStringUTF(env, "Hello from JNI!");
