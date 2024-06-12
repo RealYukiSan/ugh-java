da426f007fedc976d7f809b409f07e5f72b3e751
RealYukiSan <yukisan@d.mail>
2024-06-12 08:18:43 +0700

    update

--- hello-jni/jni/hello-alt.c
+++
@@ -1,7 +0,0 @@
-// alternative-way
-
-#include "../java/hemlo_HelloJni.h"
-
-JNIEXPORT char* JNICALL Java_HelloJNI_stringFromJNI(JNIEnv *env, jobject obj) {
-    return "Hello, World from C!\n";
-}
--- hello-jni/jni/hello-jni.c
+++ hello-jni/jni/hello-jni.c
@@ -1,8 +1,20 @@
 #include <jni.h>
 
+#define MESSAGE "Hello from JNI!"
+
 // did you wonder why the name sucks?
 // read this: https://stackoverflow.com/questions/32470463/what-is-the-naming-convention-for-java-native-interface-method-and-module-name
 jstring Java_com_example_hellojni_HelloJni_stringFromJNI(JNIEnv* env, jobject thiz)
 {
-  return (*env)->NewStringUTF(env, "Hello from JNI!");
+  return (*env)->NewStringUTF(env, MESSAGE);
+}
+
+jstring Java_hemlo_HelloJniKt_stringFromJNI(JNIEnv* env, jobject thiz)
+{
+  return (*env)->NewStringUTF(env, MESSAGE);
+}
+
+jstring Java_hemlo_HelloJni_stringFromJNI(JNIEnv* env, jobject thiz)
+{
+  return (*env)->NewStringUTF(env, MESSAGE);
 }
